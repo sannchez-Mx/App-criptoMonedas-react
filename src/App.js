4fe4714df63b7f720ab690e3af8941141564c6e4
sannchez-Mx <sannchez-mx@outlook.com>
2019-07-23 18:51:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import imagen from './cryptomonedas.png';
+
+import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
+import Spinner from './components/Spinner';
 
 function App() {
+
+  const [ moneda, guardarMoneda] = useState('');
+  const [ criptomoneda, guardarCriptomoneda] = useState('');
+  const [ cargando, guardarCargando ] = useState(false);
+  const [ resultado, guardarResultado ] = useState({});
+
+  useEffect(() => {
+      const cotizarCriptomoneda = async () => {
+
+        // si no hay moneda, no ejecutar
+        if(moneda === '') return;
+
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+        const resultado = await axios.get(url);
+
+        // mostrar spinner
+        guardarCargando(true);
+
+        // ocultar spinner y agregar el resultado
+        setTimeout(() => {
+          guardarCargando(false);
+          guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+        }, 3000);
+
+      }
+
+      cotizarCriptomoneda();
+  }, [ criptomoneda, moneda ]);
+
+  // Mostrar Spinner o resultado
+  const componente = (cargando) ? <Spinner /> : <Cotizacion resultado={resultado} />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="row">
+            <div className="one-half column">
+                <img src={imagen} alt="imagen criptomonedas" className="logotipo" />
+            </div>
+            <div className="one-half column">
+                <h1>Cotiza Criptomonedas al Instante</h1>
+
+                <Formulario 
+                  guardarMoneda={guardarMoneda}
+                  guardarCriptomoneda={guardarCriptomoneda}
+                />
+
+                {componente}
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Criptomoneda from './Criptomoneda';
+import Error from './Error';
+
+function Formulario({guardarMoneda, guardarCriptomoneda}) {
+    const [ criptomonedas, guardarCriptomonedas ] = useState([]);
+    const [ monedaCotizar, guardarMonedaCotizar ] = useState('');
+    const [ criptoCotizar, guardarCriptoCotizar ] = useState('');
+    const [ error, guardarError] = useState(false);
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+
+            // colocar respuesta en el state
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
+
+    // Validar que el usuario llene ambos campos
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        // validar si ambos campos estan llenos
+        if(monedaCotizar === '' || criptoCotizar === '') {
+            guardarError(true);
+            return;
+        }
+
+        // pasar los datos al componente principal
+        guardarError(false);
+        guardarMoneda(monedaCotizar);
+        guardarCriptomoneda(criptoCotizar);
+    }
+
+    // Mostrar el error en caso de que exista
+    const componente = (error) ?  <Error mensaje="Ambos campos son obligatorios" /> : null; 
+
+    return (
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {componente}
+            <div className="row">
+                <label>Elige tu Moneda</label>
+                <select
+                    className="u-full-width"
+                    onChange={ e => guardarMonedaCotizar(e.target.value) }
+                >
+                    <option value="">- Elige tu Moneda -</option>
+                    <option value="USD">Dolar Estadounidense</option>
+                    <option value="MXN">Peso Mexicano</option>
+                    <option value="GBP">Libras</option>
+                    <option value="EUR">Euro</option>
+                </select>
+            </div>
+
+            <div className="row">
+                <label>Elige tu Criptomoneda</label>
+                <select
+                    className="u-full-width"
+                    onChange={ e => guardarCriptoCotizar(e.target.value) }
+                >
+                    <option value="">- Elige tu Criptomoneda -</option>
+                    { criptomonedas.map(criptomoneda => (
+                        <Criptomoneda 
+                            key={criptomoneda.CoinInfo.Id}
+                            criptomoneda={criptomoneda}
+                        />
+                    ))}
+
+                </select>
+            </div>
+
+            <input type="submit" className="button-primary u-full-width" value="Calcular" />
+
+        </form>
+    )
+}
+export default Formulario;
